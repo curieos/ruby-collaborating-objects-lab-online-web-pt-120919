a8ae389cfb102fd616b42fd4ae9fd470dbafeea5
Caroline Longnecker <cvrieous@gmail.com>
2020-01-08 06:16:49 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -10,7 +10,7 @@ class Artist
   end
   
   def add_song(song)
-    return if @songs.include? { |song|  }
+    return if @songs.include? { |check_song| check_song.name == song.name }
     @songs << song
     song.artist = self
   end
