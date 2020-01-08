e06a46ce49b13e69e961b655fa37d24b0eda3c61
Caroline Longnecker <cvrieous@gmail.com>
2020-01-08 06:21:56 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -10,6 +10,7 @@ class Artist
   end
   
   def add_song(song)
+    puts song.name
     return if @songs.any? { |existing_song| existing_song.name == song.name }
     @songs << song
     song.artist = self
